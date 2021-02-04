66a7c4325d46a36f66aeeffa7c98c8809a4c8089
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-02-04 16:38:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,14 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+top_row_win = [0,1,2]
+]
+
+def won?
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  "X won in the top row"
+  elsif board[top_row_win[0]] == "O" && board[top_row_win[1]] == "O" && board[top_row_win[2]] == "O"
+  "O won in the top row"
+  end
+end
