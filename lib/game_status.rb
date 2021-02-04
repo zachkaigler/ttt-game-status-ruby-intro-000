6ac027cd81c895cb315864b9135cfee22e9d128e
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-02-04 16:58:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,21 +5,26 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #Top Row
-  [3,4,5], #Mid Row
-  [6,7,8], #Bottom Row
-  [0,4,8], #Horiz L>R
-  [2,4,6], #Horiz R>L
-  [0,3,6], #L Column
-  [1,4,7], #Mid Column
-  [2,5,8]  #R Column
+  top_row_win = [0,1,2], #Top Row
+  mid_row_win = [3,4,5], #Mid Row
+  bot_row_win = [6,7,8], #Bottom Row
+  l_hor_win = [0,4,8], #Horiz L>R
+  r_hor_win = [2,4,6], #Horiz R>L
+  l_col_win = [0,3,6], #L Column
+  mid_col_win = [1,4,7], #Mid Column
+  r_col_win = [2,5,8]  #R Column
 ]
 
-#def won?
+
+
+
+#def won?(board)
 #  top_row_win = [0,1,2]
 #  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-#  "X won in the top row"
+#    "X won in the top row"
 #  elsif board[top_row_win[0]] == "O" && board[top_row_win[1]] == "O" && board[top_row_win[2]] == "O"
-#  "O won in the top row"
+#    "O won in the top row"
+#  else
+#    false
 #  end
 #end
