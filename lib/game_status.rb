db8c435d211dc2de582884aac2718087bf43ea95
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-02-04 17:01:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,14 @@ WIN_COMBINATIONS = [
 
 
 def won?(board)
+  top_row_win = [0,1,2], #Top Row
+  mid_row_win = [3,4,5], #Mid Row
+  bot_row_win = [6,7,8], #Bottom Row
+  l_hor_win = [0,4,8], #Horiz L>R
+  r_hor_win = [2,4,6], #Horiz R>L
+  l_col_win = [0,3,6], #L Column
+  mid_col_win = [1,4,7], #Mid Column
+  r_col_win = [2,5,8]
   if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
     [0,1,2]
   elsif board[top_row_win[0]] == "O" && board[top_row_win[1]] == "O" && board[top_row_win[2]] == "O"
